142e41012818df1b63f8040b4bc45e2af6aab5ea
gkthiruvathukal <gkthiruvathukal@users.noreply.github.com>
2023-05-01 03:34:00 +0000

    deploy: a21ee06540acf1227325f90ce388fc3ba07c70a7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '30 Apr 2023',
+    VERSION: '01 May 2023',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
