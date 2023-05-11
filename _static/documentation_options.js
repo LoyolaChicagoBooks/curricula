b9dd3e6fdbe7dc38b0885dc789b7b0a41a4c422b
klaeufer <klaeufer@users.noreply.github.com>
2023-05-11 20:08:31 +0000

    deploy: 1b910587418c9f1ba79d6f7e029ede3cfd7a136c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '09 May 2023',
+    VERSION: '11 May 2023',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
